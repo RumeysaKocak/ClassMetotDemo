536fbb59599f8902b817ef1cd40d004718c7cd0f
RumeysaKocak <rumeysaeregli@gmail.com>
2021-01-22 23:29:40 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAd + musteri.MusteriSoyad + " eklendi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAd + musteri.MusteriSoyad + " silindi.");
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriId);
+                Console.WriteLine(musteri.MusteriAd);
+                Console.WriteLine(musteri.MusteriSoyad);
+
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAd = "Rumeysa";
+            musteri1.MusteriSoyad = "Koçak";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 3;
+            musteri2.MusteriAd = "Yavuz";
+            musteri2.MusteriSoyad = "Kalkan";
+
+            Musteri[] musteri = new Musteri[] { musteri1, musteri2 };
+
+            for (int i = 0; i < musteri.Length; i++)
+            {
+                Console.Write(musteri[i].MusteriId + "-)");
+                Console.WriteLine("Müşteri Adı : " + musteri[i].MusteriAd);
+                Console.WriteLine("Müşteri Soyadı: " + musteri[i].MusteriSoyad);
+            }
+
+            
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+        
+
+            musteriManager.Sil(musteri2);
+         
+
+            Musteri[] musteriList = new Musteri[] { musteri1, musteri2 };
+            musteriManager.Listele(musteriList);
+        }
+    }
+}
